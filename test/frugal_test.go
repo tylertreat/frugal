e77545a0772a68fb6ba497d8d3ccd5ad1e462756
Tyler Treat <tyler.treat@webfilings.com>
2015-09-17 17:05:19 -0500

    Add tests

---
+++ test/frugal_test.go
@@ -0,0 +1,76 @@
+package test
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/Workiva/frugal/compiler"
+)
+
+const (
+	outputDir   = "out"
+	delim       = "."
+	validFile   = "valid"
+	invalidFile = "invalid"
+)
+
+var (
+	languages           = []string{"go"}
+	expectedOutputPaths = map[string]string{
+		"go": "frug_valid.go.txt",
+	}
+)
+
+func TestValid(t *testing.T) {
+	for _, language := range languages {
+		testLanguage(t, language)
+	}
+}
+
+func TestInvalid(t *testing.T) {
+	if compiler.Compile(invalidFile, languages[0], outputDir, delim) == nil {
+		t.Fatal("Expected error")
+	}
+}
+
+func testLanguage(t *testing.T, language string) {
+	if err := compiler.Compile(validFile, language, outputDir, delim); err != nil {
+		t.Fatal("Unexpected error", err)
+	}
+	outputPath := filepath.Join(outputDir, validFile, fmt.Sprintf("frug_%s.%s", validFile, language))
+	expectedPath := expectedOutputPaths[language]
+	compareFiles(t, expectedPath, outputPath)
+}
+
+func compareFiles(t *testing.T, expectedPath, generatedPath string) {
+	expected, err := os.Open(expectedPath)
+	if err != nil {
+		t.Fatalf("Failed to open file %s", expectedPath)
+	}
+	defer expected.Close()
+	generated, err := os.Open(generatedPath)
+	if err != nil {
+		t.Fatalf("Failed to open file %s", generatedPath)
+	}
+	defer generated.Close()
+
+	expectedScanner := bufio.NewScanner(expected)
+	generatedScanner := bufio.NewScanner(generated)
+	line := 1
+	for expectedScanner.Scan() {
+		generatedScanner.Scan()
+		expectedLine := expectedScanner.Text()
+		generatedLine := generatedScanner.Text()
+		if expectedLine != generatedLine {
+			t.Fatalf("Expected line <%s>, generated line <%s> at line %d", expectedLine, generatedLine, line)
+		}
+		line++
+	}
+
+	if generatedScanner.Scan() {
+		t.Fatal("Generated has more lines than expected")
+	}
+}
