63ded83c508729aba65440a756dd075c437773f8
Steven Osborne <steven.osborne@webfilings.com>
2015-09-24 13:13:56 -0500

    Port over dart lib from frugal

---
+++ lib/frugal.dart
@@ -0,0 +1,14 @@
+library frugal;
+
+import "dart:async";
+import "dart:convert";
+import "dart:typed_data";
+
+import "package:thrift/thrift.dart";
+import "package:messaging_sdk/messaging_sdk.dart";
+
+part 'src/provider.dart';
+part 'src/subscription.dart';
+part 'src/transport.dart';
+part 'src/nats_thrift_transport.dart';
+part 'src/nats_transport.dart';
