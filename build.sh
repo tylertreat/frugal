15e8d51d15cbbf32506f04d4f2f73fa9635d4bab
Beau Lyddon <lyddonb@gmail.com>
2015-10-29 14:06:31 -0600

    Add a build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+GREEN='\e[1;32m'
+RESET='\e[0m'
+
+echo
+echo -e $GREEN"Build information"
+echo -e "--------------------------------------------------------------------------------"$RESET
+
+echo "go env"
+go env | sed "s/^/    /"
+echo
+
+echo "gitconfig"
+git config -l | sed "s/^/    /"
+echo
+
+set -e
+
+# Run 
+go run
