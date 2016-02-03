88808feff1c3f0da5230c981877addcb34699669
Steven Osborne <steven.osborne@webfilings.com>
2016-02-03 10:12:05 -0600

    Add helper script to update versions

---
+++ scripts/update_version.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# WARNING: ONLY RUN THIS FROM ROOT IN THE FRUGAL DIRECTORY!!
+# WARNING: COMMIT AND PUSH ANY LOCAL CHANGES TO BE SAFE!!
+# THIS IS WRITTEN FOR OSX, IF ON LINUX REMOVE THE '' AFTER -i IN THE 
+# REPLACE FUNCTION
+
+CURRENT=$1
+NEXT=$2
+ROOT=$PWD
+
+function replace {
+    find . -type f -exec sed -i '' 's/'$CURRENT'/'$NEXT'/g' {} +
+}
+    
+# Change versions in the compiler
+cd $ROOT/compiler
+replace
+
+# Change versions in the lib
+cd $ROOT/lib
+replace
+
+# Change versions in the test
+cd $ROOT/test
+replace
+
+# Change versions in the example
+cd $ROOT/example
+replace
+
+# Return home, your work is done!
+cd $ROOT
