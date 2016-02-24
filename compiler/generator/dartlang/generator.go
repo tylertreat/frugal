bba7168d06a13478f5f95db9e573952286db092e
Steven Osborne <steven.osborne@webfilings.com>
2016-02-24 14:25:02 -0700

    Bump version to 1.0.2

--- compiler/generator/dartlang/generator.go
+++ compiler/generator/dartlang/generator.go
@@ -102,7 +102,7 @@ func (g *Generator) addToPubspec(dir string) error {
 	}
 
 	if g.Frugal.ContainsFrugalDefinitions() {
-		deps["frugal"] = dep{Git: gitDep{URL: "git@github.com:Workiva/frugal-dart.git", Ref: "1.0.1"}}
+		deps["frugal"] = dep{Git: gitDep{URL: "git@github.com:Workiva/frugal-dart.git", Ref: "1.0.2"}}
 	}
 
 	includesSet := make(map[string]bool)
