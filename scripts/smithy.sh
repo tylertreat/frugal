4b2a7f3bcb6cbaca5a4397ca80287bb485dbef2b
Tyler Treat <tyler.treat@webfilings.com>
2016-03-30 17:11:27 -0500

    Remove dart smithy.yml and put it in top-level.
    Also, point to new messaging smithy-runner.

--- scripts/smithy.sh
+++ scripts/smithy.sh
@@ -6,10 +6,15 @@ set -o pipefail
 set -e
 
 ROOT=$PWD
+CODECOV_TOKEN='bQ4MgjJ0G2Y73v8JNX6L7yMK9679nbYB'
+THRIFT_TAG=0.9.3-wk-2
+THRIFT=thrift-$THRIFT_TAG-linux-amd64
+
 
 # Retrieve the thrift binary
 mkdir -p $ROOT/bin
-wget -O $ROOT/bin/thrift https://github.com/stevenosborne-wf/thrift/releases/download/0.9.3-wk-2/thrift-0.9.3-wk-2-linux-amd64 
+curl -L -O https://github.com/stevenosborne-wf/thrift/releases/download/$THRIFT_TAG/$THRIFT
+mv $THRIFT $ROOT/bin/thrift
 chmod 0755 $ROOT/bin/thrift
 export PATH=$PATH:$ROOT/bin
 
@@ -21,6 +26,16 @@ mv target/frugal-*.jar $ROOT
 cd $ROOT/lib/go
 go get -d ./go .
 go build
+
+# Compile the dart library code
+cd $ROOT/lib/dart
+pub get
+pub run dart_dev test
+pub run dart_dev coverage --no-html
+./tool/codecov.sh
+pub run dart_dev format --check
+pub run dart_dev analyze
+zip -r frugal-dart.zip .
 
 # Run the generator tests
 cd $ROOT
