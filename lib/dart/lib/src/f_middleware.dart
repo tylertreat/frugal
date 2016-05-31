045f76b420dc50485f98b80e9ff2051a44a0a4f0
Brian Shannan <brian.shannan@workiva.com>
2016-05-31 11:32:13 -0500

    dart: middleware documentation

--- lib/dart/lib/src/f_middleware.dart
+++ lib/dart/lib/src/f_middleware.dart
@@ -1,9 +1,20 @@
 part of frugal;
 
+/// InvocationHandler processes a method invocation on a proxied method and
+/// returns the result. The arguments should match the arity of the proxied
+/// method, and have the same types. The first argument will always be the
+/// FContext.
 typedef Object InvocationHandler(
     String serviceName, String methodName, List<Object> args);
+
+/// Middleware is used to implement interceptor logic around API calls. This can
+/// be used, for example, to implement retry policies on service calls, logging,
+/// telemetry, or authentication and authorization. Middleware me be applied to
+/// both RPC services and pub/sub scopes. Middleware returns an
+/// InvocationHandler which proxies the given InvocationHandler.
 typedef InvocationHandler Middleware(InvocationHandler);
 
+/// FMethod contains an InvocationHandler used to proxy the given service method
 class FMethod {
   String _serviceName;
   String _methodName;
@@ -16,10 +27,13 @@ class FMethod {
     this._handler = _composeMiddleware(f, middleware);
   }
 
+  /// Call invokes the proxied InvocationHandler with the given arguments
+  /// and returns the results.
   Object call(List<Object> args) {
     return this._handler(this._serviceName, this._methodName, args);
   }
 
+  /// ComposeMiddleware applies the Middleware to the provided method.
   InvocationHandler _composeMiddleware(f, List<Middleware> middleware) {
     InvocationHandler handler = (serviceName, methodName, args) {
       return Function.apply(f, args);
