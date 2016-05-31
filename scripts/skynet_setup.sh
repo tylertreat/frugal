b7a82979ba844e2da1b823bffde40d4370b8cde0
Alden Peterson <alden.peterson@workiva.com>
2016-05-31 15:51:57 -0500

    Update runner and move some skynet.yaml to shell script"

--- scripts/skynet_setup.sh
+++ scripts/skynet_setup.sh
@@ -2,11 +2,6 @@
 
 set -ex
 
-# Grab thrift binary
-mkdir -p $PWD/bin
-wget -O $PWD/bin/thrift https://github.com/stevenosborne-wf/thrift/releases/download/0.9.3-wk-2/thrift-0.9.3-wk-2-linux-amd64
-chmod 0755 $PWD/bin/thrift
-export PATH=$PATH:$PWD/bin
 testing=$PWD
 
 mkdir -p $GOPATH/src/github.com/Workiva
