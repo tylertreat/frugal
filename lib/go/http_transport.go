2d6a87dee2df9198b89e3cfa4673b40edb820ffd
Steven Osborne <steven.osborne@webfilings.com>
2016-06-16 11:24:43 -0500

    Go: Start work on http client transport

--- lib/go/http_transport.go
+++ lib/go/http_transport.go
@@ -8,11 +8,12 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"sync"
 
 	"git.apache.org/thrift.git/lib/go/thrift"
 )
 
-const payloadLimitHeader = "X-Frugal-Payload-Limit"
+const payloadLimitHeader = "x-frugal-payload-limit"
 
 var newEncoder = func(buf *bytes.Buffer) io.WriteCloser {
 	return base64.NewEncoder(base64.StdEncoding, buf)
@@ -22,7 +23,7 @@ var newEncoder = func(buf *bytes.Buffer) io.WriteCloser {
 func NewFrugalHandlerFunc(processor FProcessor, inPfactory, outPfactory *FProtocolFactory) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/x-frugal")
+		w.Header().Add("content-type", "application/x-frugal")
 
 		// Check for size limitation
 		limitStr := r.Header.Get(payloadLimitHeader)
@@ -99,4 +100,182 @@ func NewFrugalHandlerFunc(processor FProcessor, inPfactory, outPfactory *FProtoc
 
 		w.Write(encoded.Bytes())
 	}
+}
+
+// httpTTransport implements thrift.TTransport. This is a "stateless"
+// transport in the sense that there is no connection with a server. A request
+// is simply an http request and a response is an http response. This assumes
+// requests/responses fit within a single http request.
+type httpTTransport struct {
+	client            *http.Client
+	url               string
+	requestSizeLimit  uint
+	responseSizeLimit uint
+	frameBuffer       chan []byte
+	currentFrame      []byte
+	requestBuffer     *bytes.Buffer
+	mu                sync.RWMutex
+	closeChan         chan struct{}
+	isOpen            bool
+}
+
+// NewHttpTTransport returns a new Thrift TTransport which uses the
+// HTTP as the underlying transport. This TTransport is stateless in that
+// there is no connection maintained between the client and server. A request
+// is simply an http request and a response is an http response. This assumes
+// requests/responses fit within a single http request.
+func NewHttpTTransport(client *http.Client, url string) thrift.TTransport {
+	return &httpTTransport{
+		frameBuffer:   make(chan []byte, frameBufferSize),
+		requestBuffer: new(bytes.Buffer),
+	}
+}
+
+// Open initializes the close channel and sets the open flag to true.
+func (h *httpTTransport) Open() error {
+	h.mu.Lock()
+	h.closeChan = make(chan struct{})
+	h.isOpen = true
+	h.mu.Unlock()
+	return nil
+}
+
+func (h *httpTTransport) handler(data []byte) {
+	select {
+	case h.frameBuffer <- data:
+	case <-h.closeChan:
+	}
+}
+
+func (h *httpTTransport) IsOpen() bool {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+	return h.isOpen
+}
+
+// Close closes the close channel and sets the open flag to false.
+func (h *httpTTransport) Close() error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	if !h.isOpen {
+		return nil
+	}
+	close(h.closeChan)
+	h.isOpen = false
+	return nil
+}
+
+func (h *httpTTransport) Read(buf []byte) (int, error) {
+	if !h.IsOpen() {
+		return 0, h.getClosedConditionError("read:")
+	}
+	if h.currentFrame == nil {
+		select {
+		case frame := <-h.frameBuffer:
+			h.currentFrame = frame
+		case <-h.closeChan:
+			return 0, thrift.NewTTransportExceptionFromError(io.EOF)
+		}
+	}
+	num := copy(buf, h.currentFrame)
+	// TODO: We could be more efficient here. If the provided buffer isn't
+	// full, we could attempt to get the next frame.
+
+	h.currentFrame = h.currentFrame[num:]
+	if len(h.currentFrame) == 0 {
+		// The entire frame was copied, clear it.
+		h.currentFrame = nil
+	}
+	return num, nil
+}
+
+// Write the bytes to a buffer. Returns ErrTooLarge if the buffer exceeds the
+// client specified request size limit.
+func (h *httpTTransport) Write(buf []byte) (int, error) {
+	if !h.IsOpen() {
+		return 0, h.getClosedConditionError("write:")
+	}
+	if h.requestSizeLimit > 0 && uint(len(buf)+h.requestBuffer.Len()) > h.requestSizeLimit {
+		h.requestBuffer.Reset()
+		return 0, ErrTooLarge
+	}
+	num, err := h.requestBuffer.Write(buf)
+	return num, thrift.NewTTransportExceptionFromError(err)
+}
+
+// Flush sends the buffered bytes over HTTP.
+func (h *httpTTransport) Flush() error {
+	if !h.IsOpen() {
+		return h.getClosedConditionError("flush:")
+	}
+	defer h.requestBuffer.Reset()
+	data := h.requestBuffer.Bytes()
+	if len(data) == 0 {
+		return nil
+	}
+
+	response, err := h.makeRequest(data)
+	if err != nil {
+		return thrift.NewTTransportExceptionFromError(err)
+	}
+
+	select {
+	case h.frameBuffer <- response:
+	case <-h.closeChan:
+	}
+
+	return nil
+}
+
+func (h *httpTTransport) makeRequest(requestPayload []byte) ([]byte, error) {
+	// Encode request payload
+	encoded := new(bytes.Buffer)
+	encoder := newEncoder(encoded)
+	if _, err := encoder.Write(requestPayload); err != nil {
+		return nil, err
+	}
+	if err := encoder.Close(); err != nil {
+		return nil, err
+	}
+
+	// Initialize request
+	request, err := http.NewRequest("POST", h.url, encoded)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add request headers
+	request.Header.Add("content-type", "application/x-frugal")
+	request.Header.Add("accept", "application/x-frugal")
+	if h.responseSizeLimit > 0 {
+		request.Header.Add(payloadLimitHeader, strconv.FormatUint(uint64(h.responseSizeLimit), 10))
+	}
+
+	// Make request
+	response, err := h.client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode == http.StatusRequestEntityTooLarge {
+		return nil, thrift.NewTTransportException(RESPONSE_TOO_LARGE,
+			"request was too large for the transport")
+	}
+	if response.StatusCode >= 300 {
+		// TODO: Inject payload body into error
+		return nil, thrift.NewTTransportException(thrift.UNKNOWN_TRANSPORT_EXCEPTION, "")
+	}
+
+	// TODO: decode and return response payload
+
+	return nil, nil
+}
+
+func (h *httpTTransport) RemainingBytes() uint64 {
+	return ^uint64(0)
+}
+
+func (h *httpTTransport) getClosedConditionError(prefix string) error {
+	return thrift.NewTTransportException(thrift.NOT_OPEN,
+		fmt.Sprintf("%s HTTP TTransport not open", prefix))
 }
