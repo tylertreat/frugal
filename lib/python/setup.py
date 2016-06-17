c055cd17c5a619dd9e97aefadde5e41898486c2e
Charlie Strawn <charlie.strawn@workiva.com>
2016-06-17 14:41:09 -0500

    Python: Add install_requires thrift to setup.py.

--- lib/python/setup.py
+++ lib/python/setup.py
@@ -10,6 +10,9 @@ setup(
     maintainer_email='messaging@workiva.com',
     url='http://github.com/Workiva/frugal',
     packages=find_packages(exclude=('frugal.tests', 'frugal.tests.*')),
+    install_requires=[
+        'thrift==0.9.3'
+    ],
     extras_require={
         'tornado': ["nats-client==0.2.4"]
     }
