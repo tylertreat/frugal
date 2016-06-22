36807f835df19775b7461e3724b6033d3cda301f
Matthew Belisle <matthew.belisle@webfilings.com>
2016-06-22 11:36:51 -0400

    Using TSerializer

--- lib/dart/lib/src/internal/f_obj_to_json.dart
+++ lib/dart/lib/src/internal/f_obj_to_json.dart
@@ -1,38 +1,10 @@
 part of frugal;
 
-class _InMemoryTransport extends TTransport {
-  final List<int> _buffer;
-
-  _InMemoryTransport(this._buffer);
-
-  @override
-  get isOpen => true;
-
-  @override
-  Future open() {}
-
-  @override
-  Future close() {}
-
-  @override
-  Future flush() {}
-
-  @override
-  int read(Uint8List buffer, int offset, int length) {}
-
-  /// Writes into this transport's buffer from [buffer].
-  @override
-  void write(Uint8List buffer, int offset, int length) =>
-      _buffer.addAll(buffer.sublist(offset, offset + length));
-}
-
 String fObjToJson(Object obj) {
   if (obj is TBase) {
-    List<int> buf = [];
-    var tt = new _InMemoryTransport(buf);
-    var prot = new TJsonProtocol(tt);
-    obj.write(prot);
-    return new String.fromCharCodes(buf);
+    TSerializer serializer =
+        new TSerializer(protocolFactory: new TJsonProtocolFactory());
+    return new String.fromCharCodes(serializer.write(obj));
   }
   if (obj is FContext) {
     return JSON.encode(obj.requestHeaders());
