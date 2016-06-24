c352d600502a64780bbab0db6e840f3b15b8141f
johnkirchner-wf <john.kirchner@workiva.com>
2016-06-24 15:48:20 -0500

    Server fixes, modify length func f_bounded_memory_buffer

--- lib/python/frugal/tornado/transport/f_bounded_memory_buffer.py
+++ lib/python/frugal/tornado/transport/f_bounded_memory_buffer.py
@@ -4,7 +4,7 @@ from thrift.transport.TTransport import TMemoryBuffer
 _NATS_MAX_MESSAGE_SIZE = 1024 * 1024
 
 
-class FBoundedMemoryBuffer(object, TMemoryBuffer):
+class FBoundedMemoryBuffer(TMemoryBuffer, object):
 
     def __init__(self, value=None):
         super(FBoundedMemoryBuffer, self).__init__(value)
@@ -22,13 +22,13 @@ class FBoundedMemoryBuffer(object, TMemoryBuffer):
         return self._buffer.read(length)
 
     def write(self, buf):
-        if len(self._buffer.getvalue()) + len(buf.getvalue()) > _NATS_MAX_MESSAGE_SIZE:
+        if len(self) + len(buf) > _NATS_MAX_MESSAGE_SIZE:
             self._buffer = TMemoryBuffer(_NATS_MAX_MESSAGE_SIZE)
             raise FMessageSizeException("Buffer size reached {}".format(_NATS_MAX_MESSAGE_SIZE))
         self._buffer.write(buf)
 
-    def length(self):
-        return len(self._buffer.getvalue())
+    def __len__(self):
+        return len(self.getvalue())
 
     def getvalue(self):
         return self._buffer.getvalue()
