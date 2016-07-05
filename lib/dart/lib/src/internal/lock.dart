756e8aecd35db24bc6288f594a89b2f88e95cb00
Brian Shannan <brian.shannan@workiva.com>
2016-07-05 17:24:58 -0500

    dart: docstrings

--- lib/dart/lib/src/internal/lock.dart
+++ lib/dart/lib/src/internal/lock.dart
@@ -1,8 +1,14 @@
 part of frugal;
 
+/// Lock acts like a mutex, disallowing execution of critical sections of code
+/// concurrently. A `Completer` can be incorrect because all sleeping threads
+/// are woken when the completer completes, allowing multiple threads to
+/// enter the critical section. This class addresses that problem.
 class Lock {
   Completer _lock = null;
 
+  /// Locks the mutex, disallowing another thread from acquiring the lock
+  /// until unlock() is called.
   Future lock() async {
     while (_lock != null && !_lock.isCompleted) {
       await _lock.future;
@@ -10,6 +16,7 @@ class Lock {
     _lock = new Completer();
   }
 
+  /// Unlocks the mutex.
   void unlock() {
     _lock.complete();
   }
