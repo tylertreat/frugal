3eb4e7d63614641609715f965b0da5f299bbc542
Brian Shannan <brian.shannan@workiva.com>
2016-07-15 11:25:37 -0500

    python: tornado http handler

---
+++ lib/python/frugal/tests/tornado/server/test_http_handler.py
@@ -0,0 +1,71 @@
+import base64
+import mock
+
+from thrift.protocol import TBinaryProtocol
+from tornado.testing import AsyncHTTPTestCase
+from tornado.web import Application
+
+from frugal.protocol import FProtocolFactory
+from frugal.tornado.server import FrugalHttpHandler
+
+
+class TestFTornadoHTTPHandler(AsyncHTTPTestCase):
+
+    def setUp(self):
+        self.processor = mock.Mock()
+        super(TestFTornadoHTTPHandler, self).setUp()
+
+    def get_app(self):
+        prot_factory = FProtocolFactory(
+                TBinaryProtocol.TBinaryProtocolFactory())
+        return Application([
+            ('/frugal', FrugalHttpHandler, {
+                'processor': self.processor,
+                'protocol_factory': prot_factory,
+            })
+        ])
+
+    def test_basic(self):
+        request_data = bytearray([2, 3, 4])
+        request_frame = bytearray([0, 0, 0, 3]) + request_data
+        request_payload = base64.b64encode(request_frame)
+        response_data = bytearray([6, 7, 8, 9])
+        response_frame = bytearray([0, 0, 0, 4]) + response_data
+
+        def process_data(_, oprot):
+            oprot.get_transport().write(response_data)
+        self.processor.process.side_effect = process_data
+
+        response = self.fetch('/frugal', method='POST', body=request_payload)
+        self.assertEqual(200, response.code)
+        self.assertTrue(self.processor.process.called)
+        iprot, _ = self.processor.process.call_args[0]
+        self.assertEqual(request_data, iprot.get_transport().getvalue())
+
+        expected_response_payload = base64.b64encode(response_frame)
+
+        self.assertEqual(expected_response_payload, response.body)
+        self.assertEqual('application/x-frugal',
+                         response.headers['content-type'])
+        self.assertEqual('base64',
+                         response.headers['content-transfer-encoding'])
+
+    def test_response_too_large(self):
+        request_data = bytearray([2, 3, 4])
+        request_frame = bytearray([0, 0, 0, 3]) + request_data
+        request_payload = base64.b64encode(request_frame)
+        response_data = bytearray([6, 7, 8, 9, 10, 11])
+
+        def process_data(_, oprot):
+            oprot.get_transport().write(response_data)
+        self.processor.process.side_effect = process_data
+
+        headers = {
+            'x-frugal-payload-limit': '5',
+        }
+        response = self.fetch('/frugal', method='POST', body=request_payload,
+                              headers=headers)
+        self.assertEqual(413, response.code)
+        self.assertTrue(self.processor.process.called)
+        iprot, _ = self.processor.process.call_args[0]
+        self.assertEqual(request_data, iprot.get_transport().getvalue())
---
+++ lib/python/frugal/tornado/server/http_handler.py
@@ -0,0 +1,40 @@
+import base64
+import struct
+
+from thrift.transport.TTransport import TMemoryBuffer
+from tornado.web import RequestHandler
+
+
+class FTornadoHttpHandler(RequestHandler):
+    def initialize(self, processor, protocol_factory):
+        self._processor = processor
+        self._protocol_factory = protocol_factory
+
+    def post(self):
+        self.set_header('content-type', 'application/x-frugal')
+
+        # check for response size limit
+        response_limit = 0
+        if self.request.headers.get('x-frugal-payload-limit') is not None:
+            response_limit = int(self.request.headers['x-frugal-payload-limit'])
+
+        # decode payload and process
+        payload = base64.b64decode(self.request.body)
+        iprot = self._protocol_factory.get_protocol(
+            TMemoryBuffer(payload[4:])
+        )
+        out_transport = TMemoryBuffer()
+        oprot = self._protocol_factory.get_protocol(out_transport)
+        self._processor.process(iprot, oprot)
+
+        # write back response
+        output_data = out_transport.getvalue()
+        if len(output_data) > response_limit > 0:
+            self.send_error(status_code=413)
+            return
+
+        output_data_len = struct.pack('!I', len(output_data))
+        output_payload = base64.b64encode(output_data_len + output_data)
+
+        self.set_header('content-transfer-encoding', 'base64')
+        self.write(output_payload)
