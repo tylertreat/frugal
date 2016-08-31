2d2d1b6d3991a3424db35202f067003607c8564c
Kevin Sookocheff <kevin.sookocheff@workiva.com>
2016-08-31 15:17:27 -0600

    Add basic NATS benchmarks.

--- lib/java/src/test/java/com/workiva/frugal/benchmarks/NatsBenchmark.java
+++ lib/java/src/test/java/com/workiva/frugal/benchmarks/NatsBenchmark.java
@@ -2,17 +2,26 @@ package com.workiva.frugal.benchmarks;
 
 import io.nats.client.Connection;
 import io.nats.client.ConnectionFactory;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 /**
