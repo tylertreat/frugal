aa45e398a8c74c1fa8e2f175ffbc4c7def8a9486
Alden Peterson <alden.peterson@workiva.com>
2016-09-12 13:30:58 -0500

    smithy: execute language config in parallel and fix dart tests
     Please enter the commit message for your changes. Lines starting

--- scripts/smithy.sh
+++ scripts/smithy.sh
@@ -1,69 +1,17 @@
 #!/usr/bin/env bash
 
 # This is so `tee` doesn't absorb a non-zero exit code
-set -o pipefail
-# Set -e so that we fail if an error is hit.
-set -e
+set -eo pipefail
 
-# Get godep
-which godep > /dev/null || {
-    go get github.com/tools/godep
-}
+echo $PWD
 
-ROOT=$PWD
-CODECOV_TOKEN='bQ4MgjJ0G2Y73v8JNX6L7yMK9679nbYB'
-GORACE="halt_on_error=1"
+mkdir -p $SMITHY_ROOT/test_results/
 
-# JAVA
-# Compile library code
-cd $ROOT/lib/java && mvn checkstyle:check && mvn clean verify
-mv target/frugal-*.jar $ROOT
+# Move godeps to gopath
+cp -r $FRUGAL_HOME/Godeps/_workspace/* $GOPATH/
+cp -r $FRUGAL_HOME/lib/go/Godeps/_workspace/* $GOPATH/
 
-# GO
-# Compile library code
-cd $ROOT/lib/go
-godep restore
-go build
-# Run the tests
-go test -race
+# Run each language build and tests in parallel
+cd $FRUGAL_HOME
+go run scripts/smithy/parallel_smithy.go
 
-# DART
-# Wrap up package for pub
-cd $ROOT
-tar -C lib/dart -czf frugal.pub.tgz .
-# Compile library code
-cd $ROOT/lib/dart
-pub get
-# Run the tests
-pub run dart_dev test
-pub run dart_dev coverage --no-html
-./tool/codecov.sh
-pub run dart_dev format --check
-pub run dart_dev analyze
-
-# Python
-virtualenv -p /usr/bin/python /tmp/frugal
-source /tmp/frugal/bin/activate
-pip install -U pip
-cd $ROOT/lib/python
-make deps-tornado
-make deps-gae
-make xunit-py2
-deactivate
-
-virtualenv -p /usr/bin/python3.5 /tmp/frugal-py3
-source /tmp/frugal-py3/bin/activate
-pip install -U pip
-cd $ROOT/lib/python
-make deps-asyncio
-make xunit-py3
-make install
-mv dist/frugal-*.tar.gz $ROOT
-deactivate
-
-# Run the generator tests
-cd $ROOT
-godep restore
-go build -o frugal
-go test -race ./test
-rm -rf ./test/out
---
+++ scripts/smithy/parallel_smithy.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"os/exec"
+	"sync"
+	log "github.com/Sirupsen/logrus"
+	"io/ioutil"
+	"os"
+	"fmt"
+)
+
+func main(){
+
+	testScriptDir := "scripts/smithy/"
+	// For new/removed files, update smithy.yaml to no longer print
+	testScripts := []string{"smithy_dart.sh", "smithy_go.sh", "smithy_java.sh", "smithy_generator.sh", "smithy_python.sh"}
+
+	// Allow each setup script to run concurrently
+	var wg sync.WaitGroup
+	wg.Add(len(testScripts))
+
+	for _, script := range testScripts {
+		go runTestScript(script, testScriptDir, &wg)
+	}
+
+	wg.Wait()
+
+}
+
+func runTestScript(script string, scriptDir string, wg *sync.WaitGroup){
+	fullScript := scriptDir + script
+	log.Info("Running script:", script)
+	out, err := exec.Command("/bin/bash", fullScript).CombinedOutput();
+
+	if err != nil {
+		log.Errorf("Script '%s' failed with output:\n%s", script, out)
+	}
+
+
+	logFile := os.ExpandEnv("${SMITHY_ROOT}/test_results/" + script + "_out.txt")
+	err2 := writeFile(logFile, out)
+
+	fmt.Print(out)
+	
+	if err2 != nil {
+		log.Errorf("Writing log file '%s' failed with error:%s", logFile, err2)
+	}
+
+	if err != nil || err2 != nil {
+		os.Exit(1)
+	}
+
+	log.Info("Test script complete:", script)
+	wg.Done()
+
+}
+
+func writeFile(logFile string, commandData []byte) (error) {
+
+	return ioutil.WriteFile(logFile, commandData, 0644)
+
+}
