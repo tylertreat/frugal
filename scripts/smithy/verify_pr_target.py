1df33b507074ed39bdcdf29e57e1c3812bc43cbb
Alden Peterson <alden.peterson@workiva.com>
2016-09-19 14:18:01 -0500

    smithy: update master-merge check script to whitelist hotfix branches

--- scripts/smithy/verify_pr_target.py
+++ scripts/smithy/verify_pr_target.py
@@ -7,6 +7,10 @@ def _branch_matches_release_branch(branch_name):
     return re.search("release_[0-9]+.[0-9]+.[0-9]+", branch_name)
 
 
+def _branch_matches_hotfix_branch(branch_name):
+    return re.search("hotfix_[0-9]+.[0-9]+.[0-9]+", branch_name)
+
+
 def _is_pull_request():
     return 'GIT_MERGE_BRANCH' in os.environ
 
@@ -17,7 +21,23 @@ def test_regex():
     assert(not _branch_matches_release_branch('releases_1.1.1'))
     assert(not _branch_matches_release_branch('release_1.1'))
     assert(not _branch_matches_release_branch('release_1'))
+
+    assert(_branch_matches_hotfix_branch('hotfix_1.1.1'))
+    assert(_branch_matches_hotfix_branch('hotfix_1234.1.1'))
+    assert(not _branch_matches_hotfix_branch('hotfixs_1.1.1'))
+    assert(not _branch_matches_hotfix_branch('hotfix_1.1'))
+    assert(not _branch_matches_hotfix_branch('hotfix_1'))
+
+    assert(_allow_master_merge('hotfix_1.1.1'))
+    assert(_allow_master_merge('hotfix_1234.1.1'))
+    assert(not _allow_master_merge('hotfixs_1.1.1'))
+    assert(not _allow_master_merge('hotfix_1.1'))
+    assert(not _allow_master_merge('hotfix_1'))
     
+
+def _allow_master_merge(branch_name):
+    return _branch_matches_release_branch(branch_name) or _branch_matches_hotfix_branch(branch_name)
+
 
 def main():
     """
@@ -30,7 +50,7 @@ def main():
         merge_branch = os.environ['GIT_MERGE_BRANCH']
         cur_branch = os.environ['GIT_BRANCH']
 
-        if merge_branch == 'master' and not _branch_matches_release_branch(cur_branch):
+        if merge_branch == 'master' and not _allow_master_merge(cur_branch):
             print('ERROR: Your branch:{cur_branch} does not appear to be a release PR, but was made against master'.format(cur_branch=cur_branch))
             sys.exit(1)
 
