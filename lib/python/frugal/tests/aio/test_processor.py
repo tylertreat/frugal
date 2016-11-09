0c6ae5cbe2fbb5d4e0578689b90a0e899f4b1f33
Brian Shannan <brian.shannan@workiva.com>
2016-11-09 08:16:28 -0600

    python: fix unit tests

--- lib/python/frugal/tests/aio/test_processor.py
+++ lib/python/frugal/tests/aio/test_processor.py
@@ -1,22 +1,25 @@
-import unittest
+from asyncio import Future
 from mock import Mock
 
 from thrift.protocol.TBinaryProtocol import TBinaryProtocolFactory
 from thrift.Thrift import TApplicationException
 from thrift.transport.TTransport import TMemoryBuffer
 
+from frugal.aio.processor import FBaseProcessor
 from frugal.exceptions import FException
-from frugal.processor import FBaseProcessor
 from frugal.protocol import FProtocolFactory
 from frugal.transport import TMemoryOutputBuffer
+from frugal.tests.aio import utils
 
 
-class TestFBaseProcessor(unittest.TestCase):
+class TestFBaseProcessor(utils.AsyncIOTestCase):
 
-    def test_process_processor_exception(self):
+    @utils.async_runner
+    async def test_process_processor_exception(self):
         processor = FBaseProcessor()
         proc = Mock()
-        proc.process.side_effect = FException()
+        e = FException('foo bar exception')
+        proc.process.side_effect = e
         processor.add_to_processor_map("basePing", proc)
         frame = bytearray(
             b'\x00\x00\x00\x00\x0e\x00\x00\x00\x05_opid\x00\x00\x00\x011'
@@ -25,9 +28,13 @@ class TestFBaseProcessor(unittest.TestCase):
         itrans = TMemoryBuffer(value=frame)
         iprot = FProtocolFactory(TBinaryProtocolFactory()).get_protocol(itrans)
         oprot = Mock()
-        processor.process(iprot, oprot)
 
-    def test_process_missing_function(self):
+        with self.assertRaises(FException) as cm:
+            await processor.process(iprot, oprot)
+        self.assertEqual(e, cm.exception)
+
+    @utils.async_runner
+    async def test_process_missing_function(self):
         processor = FBaseProcessor()
         frame = bytearray(
             b'\x00\x00\x00\x00\x0e\x00\x00\x00\x05_opid\x00\x00\x00\x011'
@@ -35,10 +42,10 @@ class TestFBaseProcessor(unittest.TestCase):
         )
         itrans = TMemoryBuffer(value=frame)
         iprot = FProtocolFactory(TBinaryProtocolFactory()).get_protocol(itrans)
-        otrans = TMemoryOutputBuffer(1000)
+        otrans = TMemoryOutputBuffer(100)
         oprot = FProtocolFactory(TBinaryProtocolFactory()).get_protocol(otrans)
         with self.assertRaises(TApplicationException):
-            processor.process(iprot, oprot)
+            await processor.process(iprot, oprot)
 
         expected_response = bytearray(
             b'\x00\x00\x00\x50\x00\x00\x00\x00\x0e\x00\x00\x00\x05_opid\x00\x00'
@@ -46,12 +53,15 @@ class TestFBaseProcessor(unittest.TestCase):
             b'\x0b\x00\x01\x00\x00\x00\x1aUnknown function: basePing\x08\x00'
             b'\x02\x00\x00\x00\x01\x00'
         )
-        print(len(expected_response))
         self.assertEqual(otrans.getvalue(), expected_response)
 
-    def test_process(self):
+    @utils.async_runner
+    async def test_process(self):
         processor = FBaseProcessor()
         proc = Mock()
+        future = Future()
+        future.set_result(None)
+        proc.process.return_value = future
         processor.add_to_processor_map("basePing", proc)
         frame = bytearray(
             b'\x00\x00\x00\x00\x0e\x00\x00\x00\x05_opid\x00\x00\x00\x011'
@@ -60,7 +70,7 @@ class TestFBaseProcessor(unittest.TestCase):
         itrans = TMemoryBuffer(value=frame)
         iprot = FProtocolFactory(TBinaryProtocolFactory()).get_protocol(itrans)
         oprot = Mock()
-        processor.process(iprot, oprot)
+        await processor.process(iprot, oprot)
         assert(proc.process.call_args)
         args, _ = proc.process.call_args
         assert(args[0]._get_op_id() == 1)
