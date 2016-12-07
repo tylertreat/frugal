b382a61669f025bfbbbaf2d0b36376c17a46366d
Theisen Sanders <theisen.sanders@workiva.com>
2016-12-06 17:36:33 -0700

    Support async processors in tornado http handler

--- lib/python/frugal/tests/tornado/server/test_http_handler.py
+++ lib/python/frugal/tests/tornado/server/test_http_handler.py
@@ -2,6 +2,7 @@ import base64
 import mock
 
 from thrift.protocol import TBinaryProtocol
+from tornado import gen
 from tornado.testing import AsyncHTTPTestCase
 from tornado.web import Application
 
@@ -33,6 +34,33 @@ class TestFTornadoHTTPHandler(AsyncHTTPTestCase):
         response_frame = bytearray([0, 0, 0, 4]) + response_data
 
         def process_data(_, oprot):
+            oprot.get_transport().write(response_data)
+        self.processor.process.side_effect = process_data
+
+        response = self.fetch('/frugal', method='POST', body=request_payload)
+        self.assertEqual(200, response.code)
+        self.assertTrue(self.processor.process.called)
+        iprot, _ = self.processor.process.call_args[0]
+        self.assertEqual(request_data, iprot.get_transport().getvalue())
+
+        expected_response_payload = base64.b64encode(response_frame)
+
+        self.assertEqual(expected_response_payload, response.body)
+        self.assertEqual('application/x-frugal',
+                         response.headers['content-type'])
+        self.assertEqual('base64',
+                         response.headers['content-transfer-encoding'])
+
+    def test_async(self):
+        request_data = bytearray([2, 3, 4])
+        request_frame = bytearray([0, 0, 0, 3]) + request_data
+        request_payload = base64.b64encode(request_frame)
+        response_data = bytearray([6, 7, 8, 9])
+        response_frame = bytearray([0, 0, 0, 4]) + response_data
+
+        @gen.coroutine
+        def process_data(_, oprot):
+            yield gen.moment
             oprot.get_transport().write(response_data)
         self.processor.process.side_effect = process_data
 
