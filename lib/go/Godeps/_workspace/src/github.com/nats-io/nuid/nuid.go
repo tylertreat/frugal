cddb962619bf15f4abe7b5147889d7d07534653c
Tyler Treat <tyler.treat@webfilings.com>
2016-12-15 11:15:03 -0600

    Go: update nuid dep to 1.0.0

--- lib/go/Godeps/_workspace/src/github.com/nats-io/nuid/nuid.go
+++ lib/go/Godeps/_workspace/src/github.com/nats-io/nuid/nuid.go
@@ -1,11 +1,12 @@
 // Copyright 2016 Apcera Inc. All rights reserved.
 
-// A unique identifier generator that is high performance, very fast, and entropy pool friendly.
+// A unique identifier generator that is high performance, very fast, and tries to be entropy pool friendly.
 package nuid
 
 import (
 	"crypto/rand"
 	"fmt"
+	"math"
 	"math/big"
 	"sync"
 	"time"
@@ -16,15 +17,17 @@ import (
 // NUID needs to be very fast to generate and truly unique, all while being entropy pool friendly.
 // We will use 12 bytes of crypto generated data (entropy draining), and 10 bytes of sequential data
 // that is started at a pseudo random number and increments with a pseudo-random increment.
-// Total is 22 bytes of base 36 ascii text :)
+// Total is 22 bytes of base 62 ascii text :)
+
+// Version of the library
+const Version = "1.0.0"
 
 const (
-	digits   = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	base     = 36
+	digits   = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	base     = 62
 	preLen   = 12
 	seqLen   = 10
-	maxPre   = int64(4738381338321616896) // base^preLen == 36^12
-	maxSeq   = int64(3656158440062976)    // base^seqLen == 36^10
+	maxSeq   = int64(839299365868340224) // base^seqLen == 62^10
 	minInc   = int64(33)
 	maxInc   = int64(333)
 	totalLen = preLen + seqLen
@@ -44,9 +47,15 @@ type lockedNUID struct {
 // Global NUID
 var globalNUID *lockedNUID
 
-// Seed sequential random with math/random and current time and generate crypto prefix.
+// Seed sequential random with crypto or math/random and current time
+// and generate crypto prefix.
 func init() {
-	prand.Seed(time.Now().UnixNano())
+	r, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
+	if err != nil {
+		prand.Seed(time.Now().UnixNano())
+	} else {
+		prand.Seed(r.Int64())
+	}
 	globalNUID = &lockedNUID{NUID: New()}
 	globalNUID.RandomizePrefix()
 }
@@ -100,16 +109,16 @@ func (n *NUID) resetSequential() {
 }
 
 // Generate a new prefix from crypto/rand.
-// This will drain entropy and will be called automatically when we exhaust the sequential
+// This call *can* drain entropy and will be called automatically when we exhaust the sequential range.
 // Will panic if it gets an error from rand.Int()
 func (n *NUID) RandomizePrefix() {
-	r, err := rand.Int(rand.Reader, big.NewInt(maxPre))
-	if err != nil {
+	var cb [preLen]byte
+	cbs := cb[:]
+	if nb, err := rand.Read(cbs); nb != preLen || err != nil {
 		panic(fmt.Sprintf("nuid: failed generating crypto random number: %v\n", err))
 	}
-	i := len(n.pre)
-	for l := r.Int64(); l > 0; l /= base {
-		i -= 1
-		n.pre[i] = digits[l%base]
+
+	for i := 0; i < preLen; i++ {
+		n.pre[i] = digits[int(cbs[i])%base]
 	}
 }
