673a44a70d54679e6a3577af16abf07818e02500
Charlie Strawn <charlie.strawn@workiva.com>
2016-12-15 16:08:12 -0600

    Last try.

--- scripts/smithy.sh
+++ scripts/smithy.sh
@@ -7,6 +7,11 @@ python $SMITHY_ROOT/scripts/smithy/verify_pr_target.py
 
 mkdir -p $SMITHY_ROOT/test_results/
 
+# Copy the vendored deps into the $GOPATH so we dont need
+# another Godeps workspace.
+cp -r $FRUGAL_HOME/vendor/* $GOPATH/src/
+cp -r $FRUGAL_HOME/lib/go/vendor/* $GOPATH/src
+
 # Run each language build and tests in parallel
 cd $FRUGAL_HOME
 go run scripts/smithy/parallel_smithy.go
