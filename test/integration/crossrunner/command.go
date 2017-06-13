2db461b53602e0902587a2835e7de44ee9052b0d
blakeroberts-wk <blake.roberts@workiva.com>
2017-06-13 14:14:28 -0500

    test: technically make small "optimization" to crossrunner (really just some code clean up)

--- test/integration/crossrunner/command.go
+++ test/integration/crossrunner/command.go
@@ -9,17 +9,14 @@ import (
 // getCommand returns a Cmd struct used to execute a client or server and a
 // nicely formatted string for verbose loggings
 func getCommand(config config, port int) (cmd *exec.Cmd, formatted string) {
-	var args []string
 
 	command := config.Command[0]
-	// Not sure if we need to check that the slice is longer than 1
-	args = config.Command[1:]
 
-	args = append(args, []string{
+	args := append(config.Command[1:],
 		fmt.Sprintf("--protocol=%s", config.Protocol),
 		fmt.Sprintf("--transport=%s", config.Transport),
 		fmt.Sprintf("--port=%v", port),
-	}...)
+	)
 
 	cmd = exec.Command(command, args...)
 	cmd.Dir = config.Workdir
