342fd24fa62dedbf66405dbc47d9bfd535908c4d
Charlie Strawn <charlie.strawn@workiva.com>
2017-06-16 10:20:45 -0500

    go get logrus for tests.

--- scripts/smithy.sh
+++ scripts/smithy.sh
@@ -7,5 +7,8 @@ python $SMITHY_ROOT/scripts/smithy/verify_pr_target.py
 
 mkdir -p $SMITHY_ROOT/test_results/
 
+# Logrus for testing
+go get github.com/Sirupsen/logrus
+
 # Run each language build and tests in parallel
 cd $FRUGAL_HOME && go run scripts/smithy/parallel_smithy.go
