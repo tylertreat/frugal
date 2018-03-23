9ef22b8ff2101ca28cd6fe9be4b0c5c7216a6e70
Brian Shannan <brian.shannan@workiva.com>
2018-03-23 15:08:14 -0500

    fix skynet tests

--- scripts/skynet/cross/setup_go.sh
+++ scripts/skynet/cross/setup_go.sh
@@ -10,9 +10,15 @@ fi
 
 cd $FRUGAL_HOME
 
+# setup a vendor folder with frugals dependencies and frugal
+cd test/integration/go
+mkdir vendor
+cp -r $FRUGAL_HOME/lib/go/vendor/* vendor/
+rm -rf $FRUGAL_HOME/lib/go/vendor
+mkdir -p vendor/github.com/Workiva/frugal/lib/go
+cp -r $FRUGAL_HOME/lib/go/* vendor/github.com/Workiva/frugal/lib/go
+
 # Create Go binaries
 rm -rf test/integration/go/bin/*
-cd test/integration/go
-glide install
 go build -o bin/testclient src/bin/testclient/main.go
 go build -o bin/testserver src/bin/testserver/main.go
