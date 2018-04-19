b1b088633e9f6415e848d900ef6142ef867f97cf
Brett Kail <brett.kail@workiva.com>
2018-04-18 22:36:36 -0500

    dart: Add generateDocComment
    Refactor g.GenerateInlineComment(..., tab+"/") to g.generateDocComment.

--- compiler/generator/dartlang/generator.go
+++ compiler/generator/dartlang/generator.go
@@ -371,6 +371,10 @@ func (g *Generator) GenerateDocStringComment(file *os.File) error {
 	return err
 }
 
+func (g *Generator) generateDocComment(comment []string, indent string) string {
+	return g.GenerateInlineComment(comment, indent+"/")
+}
+
 // GenerateConstantsContents generates constants.
 func (g *Generator) GenerateConstantsContents(constants []*parser.Constant) error {
 	if len(constants) == 0 {
@@ -398,7 +402,7 @@ func (g *Generator) GenerateConstantsContents(constants []*parser.Constant) erro
 	contents += fmt.Sprintf("class %s {\n", className)
 	for _, constant := range constants {
 		if constant.Comment != nil {
-			contents += g.GenerateInlineComment(constant.Comment, tab+"/")
+			contents += g.generateDocComment(constant.Comment, tab)
 		}
 		value := g.generateConstantValue(constant.Type, constant.Value, "")
 		contents += fmt.Sprintf(tab+"static final %s %s = %s;\n",
@@ -534,7 +538,7 @@ func (g *Generator) generateEnumUsingClasses(enum *parser.Enum) string {
 	contents += fmt.Sprintf("class %s {\n", enum.Name)
 	for _, field := range enum.Values {
 		if field.Comment != nil {
-			contents += g.GenerateInlineComment(field.Comment, tab+"/")
+			contents += g.generateDocComment(field.Comment, tab)
 		}
 		contents += fmt.Sprintf(tab+"static const int %s = %d;\n", field.Name, field.Value)
 	}
@@ -560,7 +564,7 @@ func (g *Generator) generateEnumUsingEnums(enum *parser.Enum) string {
 	contents += fmt.Sprintf("enum %s {\n", enum.Name)
 	for _, field := range enum.Values {
 		if field.Comment != nil {
-			contents += g.GenerateInlineComment(field.Comment, tab+"/")
+			contents += g.generateDocComment(field.Comment, tab)
 		}
 		contents += fmt.Sprintf(tab+"%s,\n", field.Name)
 	}
@@ -1376,7 +1380,7 @@ func (g *Generator) GeneratePublisher(file *os.File, scope *parser.Scope) error 
 		publishers += prefix
 		prefix = "\n\n"
 		if op.Comment != nil {
-			publishers += g.GenerateInlineComment(op.Comment, tab+"/")
+			publishers += g.generateDocComment(op.Comment, tab)
 		}
 
 		publishers += fmt.Sprintf(tab+"Future publish%s(frugal.FContext ctx, %s%s req) {\n", op.Name, args, g.getDartTypeFromThriftType(op.Type))
@@ -1455,7 +1459,7 @@ func (g *Generator) GenerateSubscriber(file *os.File, scope *parser.Scope) error
 		subscribers += prefix
 		prefix = "\n\n"
 		if op.Comment != nil {
-			subscribers += g.GenerateInlineComment(op.Comment, tab+"/")
+			subscribers += g.generateDocComment(op.Comment, tab)
 		}
 		subscribers += fmt.Sprintf(tab+"Future<frugal.FSubscription> subscribe%s(%sdynamic on%s(frugal.FContext ctx, %s req)) async {\n",
 			op.Name, args, op.Type.ParamName(), g.getDartTypeFromThriftType(op.Type))
@@ -1511,12 +1515,12 @@ func (g *Generator) GenerateService(file *os.File, s *parser.Service) error {
 func (g *Generator) generateCommentWithDeprecated(comment []string, indent string, anns parser.Annotations) string {
 	contents := ""
 	if comment != nil {
-		contents += g.GenerateInlineComment(comment, indent+"/")
+		contents += g.generateDocComment(comment, indent)
 	}
 
 	if deprecationValue, deprecated := anns.Deprecated(); deprecated {
 		if deprecationValue != "" {
-			contents += g.GenerateInlineComment([]string{"Deprecated: " + deprecationValue}, indent+"/")
+			contents += g.generateDocComment([]string{"Deprecated: " + deprecationValue}, indent)
 		}
 		contents += indent + "@deprecated\n"
 	}
